44d22d501f6665e9d277d1c55b7adf8f36156881
Perennial2002 <pendragon05082002@gmail.com>
2022-01-27 20:26:19 +0500

    Second activity added

--- app/src/main/java/com/example/appjava/MainActivity.java
+++ app/src/main/java/com/example/appjava/MainActivity.java
@@ -2,11 +2,21 @@ package com.example.appjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout textInputLayout;
+    private TextInputEditText textInputEditText;
+    private Button confirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +27,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        findViewById(R.id.btn_press).setOnClickListener(new View.OnClickListener() {
+        textInputLayout = findViewById(R.id.ttl_password);
+        textInputEditText = findViewById(R.id.edt_change_listener);
+        confirmButton = findViewById(R.id.btn_confirm);
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSecondActivity();
+                if (textInputEditText.getText().toString().isEmpty()) {
+                    textInputLayout.setError("Empty field");
+                }
+            }
+        });
+
+        textInputEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().isEmpty()) {
+                    textInputLayout.setErrorEnabled(false);
+                }
+
+                if (s.toString().length() > 12) {
+                    textInputLayout.setError("Very long");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
-
-    private void openSecondActivity() {
-        Intent intent = new Intent(this,SecondActivity.class);
-        startActivity(intent);
-    }
-
-
 }
